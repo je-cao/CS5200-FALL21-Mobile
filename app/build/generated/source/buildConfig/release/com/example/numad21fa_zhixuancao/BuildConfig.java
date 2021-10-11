5aabee77a3c5378c53560c07cc85e58c4791df37
zhixua <cao.zhix@northeastern.edu>
2021-10-10 18:41:40 -0700

    Zhixuan HW5 from branch master to main

---
+++ app/build/generated/source/buildConfig/release/com/example/numad21fa_zhixuancao/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.numad21fa_zhixuancao;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.numad21fa_zhixuancao";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
